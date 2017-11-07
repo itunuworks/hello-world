a8f5ab8157ac99a8d81433c9965267d07e7a867e
itunuworks <itunuloluwa.fatoki@gmail.com>
2017-11-07 09:57:21 +0100

    the real test

--- app/helloWorld.js
+++ app/helloWorld.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const outputText = 'Now the actual test!';
+const outputText = 'Devops is the Bomb. Like the Explosion!!!';
 app.get('/', (req, res) => res.send(outputText));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
