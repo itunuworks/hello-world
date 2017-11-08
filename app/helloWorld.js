34b559f84cc33103c561ed00f04368feb2ec231b
itunuworks <itunuloluwa.fatoki@gmail.com>
2017-11-08 16:47:57 +0100

    The true test

--- app/helloWorld.js
+++ app/helloWorld.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const outputText = 'Devops is the Bomb. Like the Explosion!!!';
+const outputText = 'Yes! We finally killed it just the way it should be done!!!';
 app.get('/', (req, res) => res.send(outputText));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
